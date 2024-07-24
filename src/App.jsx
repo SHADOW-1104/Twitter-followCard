8eb6989dea4e6aa6ff5a345615acc7f1d05085cd
SHADOW-1104 <ing.colin@outlook.com>
2024-07-24 17:19:58 -0600

    twitter followCard

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Card } from "./Card"
+import './assets/css/App.css'
+
+export const App = () => {
+  return (
+    <>
+    <div className="container">
+
+    <Card name="Mami Nanami" user="mami-chan" img="1.jpeg"/>
+    <Card name="Frieren" user="frieren-sama" img="2.jpg" />
+    <Card name="Nino Nakano" user="nakano-nino" img="3.jpg"/>
+    <Card name="Fern" user="Fern_01293" img="5.jpg"/>
+    <Card name="Chizuru Mizuhara" user="chizuru-ichinose" img="4.jpg"/>
+ 
+    </div>
+    </>
+  )
+}
+
---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+import './assets/css/Card.css'
+export const Card = ({name, user, img}) => {
+  return (
+    <>
+      <article className="tw-followCard">
+        <header className="tw-followCard-header">
+          <img
+          className="tw-followCard-avatar"
+          src={`src/assets/img/${img}`}
+          alt={`${name} Thumbnail`} />
+          <div className="tw-followCard-info">
+            <strong className="tw-followCard-infoUserName">{name}</strong>
+            <p className="tw-followCard-infoEmail">@{user}</p>
+          </div>
+        </header>
+        <aside className='tw-followCard-aside'>
+          <button className="tw-followCard-btn" type="button">Follow!</button>
+        </aside>
+      </article>
+    </>
+  )
+}
