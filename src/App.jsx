89afa7b9c26b552f4b5a7534eead8161c952b69c
SHADOW-1104 <ing.colin@outlook.com>
2024-07-24 17:34:32 -0600

    correcciones

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ export const App = () => {
     <>
     <div className="container">
 
-    <Card name="Mami Nanami" user="mami-chan" img="src\assets\img\1.jpeg"/>
-    <Card name="Frieren" user="frieren-sama" img="src\assets\img\2.jpg" />
-    <Card name="Nino Nakano" user="nakano-nino" img="src\assets\img\3.jpg"/>
-    <Card name="Fern" user="Fern_01293" img="src\assets\img\5.JPG"/>
+    <Card name="Mami Nanami" user="mami-chan" img="src/assets/img/1.jpeg"/>
+    <Card name="Frieren" user="frieren-sama" img="src/assets/img/2.jpg" />
+    <Card name="Nino Nakano" user="nakano-nino" img="src/assets/img/3.jpg"/>
+    <Card name="Fern" user="Fern_01293" img="src/assets/img/5.JPG"/>
     <Card name="Chizuru Mizuhara" user="chizuru-ichinose" img="src\assets\img\4.jpg"/>
  
     </div>
